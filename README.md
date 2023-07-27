21aee9e053bc28f11c5cd609c62aa0f02d12a74b
Michael Jegat <mike@boardscoreapp.com>
2023-07-27 15:32:50 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Introduction
+This library offers a SDK to access Stripe services for the [ES4X Runtime](https://github.com/reactiverse/es4x).
+
+Right now only a few methods have been created:
+- **Connect accounts**
+- **Checkout Session**
+
+# Usage
---
+++ src/StripeAPI.js
@@ -0,0 +1,28 @@
+import { ObjUtils } from 'es4x-utils/src/utils/ObjUtils';
+import { LogUtils } from 'es4x-utils/src/utils/LogUtils';
+import { WebClientMgr } from 'es4x-utils/src/network/WebClientMgr';
+
+class	StripeAPI
+{
+	constructor(_vertx)
+	{
+		this.__vertx = _vertx;
+		this.__webClient = null;
+	}
+
+	getWebClient()
+	{
+		// lazy load the web client only when we need it
+		if (this.__webClient == null)
+		{
+			this.__webClient = new WebClientMgr(this.__vertx);
+		}
+
+		// return it
+		return this.__webClient;		
+	}
+}
+
+module.exports = {
+	StripeAPI
+};
