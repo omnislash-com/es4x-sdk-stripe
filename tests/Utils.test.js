80f63a75a0f378a00e23c17334205d9cbc905b4f
Michael Jegat <mike@boardscoreapp.com>
2023-08-01 15:03:59 -0700

    remove

--- tests/Utils.test.js
+++ tests/Utils.test.js
@@ -14,9 +14,9 @@ const suite = TestSuite.create("ES4X Test: Utils");
 suite.test("StripeAPI.VerifySignature", function (context) {
 
 	//NOT WORKING
-	
-	let	key = "whsec_7L9HryRDS6mNmW6wiftsUOmCPQO7aVeu";
-	let	signature = "t=1690921897,v1=1b47417e05ac9118ac87ab8192e1960d81886641338580808dd878996147c4f4,v0=e7b3d6b10f5175b47fac8fe1c71c0ec67d6632f7648a01b503d28261adf5c26a";
+
+	let	key = "";
+	let	signature = "";
 	let	data = {};
 	let	isTest = true;
 
